eae2c90289a003582263bf260c2e19a5eb9a1f97
Thomas Kremel <kremelt@gmail.com>
2018-12-10 10:31:57 +0200

    [main,heap] Testing code added, many bugs removed, but there are stil some.

--- fibonacci_heap/src/fib_heap.h
+++ fibonacci_heap/src/fib_heap.h
@@ -1,234 +1,313 @@
 #ifndef FIB_HEAP_HPP
 #define FIB_HEAP_HPP
 
-#define DEBUG 0
+#define NDEBUG 0
 
-#include <utility>
 #include <cstdint>
-#include <cmath>
+#include <cassert>
+#include <cstring>
 #include <limits>
+#include <type_traits>
 
 namespace fh {
 
-template<typename T>
-class fibonacci_heap {
-public:
-	using priority_t = int_least32_t;
-	using rank_t = uint_least32_t;
-	class node;
-	using node_handle_t = node * const;
-private:
-	class node {
+	template<typename T, typename Function>
+	class fibonacci_heap {
+		class node {
+		public:
+			node* parent = nullptr;
+			node* right = this;
+			node* left = this;
+			node* child = nullptr;
+			T value;
+			int_least32_t priority = 0;
+			uint_least32_t rank = 0;
+			bool marked = false;
+
+			node() : value(T()) {}
+
+			explicit node(T&& val) : value(std::forward<T>(val)) {}
+
+			// As fast as possible (just C++ thing).
+			explicit node(const T& val) : value(val) {}
+
+			// At the moment, check that I do not do anything unexpected.
+			node(const node& other) = delete;
+			node(node&& other) noexcept = delete;
+			node& operator=(const node& other) = delete;
+			node& operator=(node&& other) noexcept = delete;
+
+			// Merge two LLs into one LL, return one random node from the resulting LL.
+			static node* merge(node* double_linked_list1, node* double_linked_list2) noexcept {
+				if (!double_linked_list1)
+					return double_linked_list2;
+				if (!double_linked_list2)
+					return double_linked_list1;
+				auto last1 = double_linked_list1->right;
+				auto last2 = double_linked_list2->left;
+				double_linked_list1->right = double_linked_list2;
+				double_linked_list2->left = double_linked_list1;
+				last1->left = last2;
+				last2->right = last1;
+				return double_linked_list2;
+			}
+
+			// Remove this from the linked list of its neighbors and return handle to the linked list.
+			node* remove_from_neighbors() noexcept {
+				// Trivial case of linked list with one element.
+				if (!has_neighbors())
+					return nullptr;
+				auto handle = right;
+				right->left = left;
+				left->right = right;
+				left = right = this;
+				return handle;
+			}
+
+			bool has_neighbors() const noexcept { return !((right == left) && (left == this)); }
+			bool is_root() const noexcept { return parent == nullptr; }
+
+			~node() {
+				delete parent;
+				if (right != this) delete right;
+				if (left != this) delete left;
+				delete child;
+			}
+		};
+
+		template<typename Action>
+		void list_foreach(node* list, Action&& action) const {
+			if (!list)
+				return;
+			auto end = list;
+			auto iterator = list;
+			for (; iterator->right != end; iterator = iterator->right) {
+				action(iterator);
+			} // Cycle stops before running action on the last node.
+			action(iterator); // Handles even list with one element.
+		}
+
+		Function& fun_;
 	public:
-		node* parent = nullptr;
-		node* right = this;
-		node* left = this;
-		node* child = nullptr;
-		T value;
-		priority_t priority = 0;
-		rank_t rank = 0;
-		bool marked = false;
+		using node_t = node*;
+		node* min_node = nullptr;
+		uint_least32_t elements_count = 0;
+		// I strove for using some template magic with SFINAE, but my compiler
+		// protested (low version with only mobile data internet access is a bad combination).
+		// If using older standard than c++17 the complexity and readability
+		// would grew too much, so I used only bool and one explicit constructor
+		// (if function is not needed one can use [](){} as input).
+		bool naive_implementation = false;
 
-		node() : value(T()) {
+		explicit fibonacci_heap(Function& f) : fun_(f) {}
+
+		// At the moment, check that I do not do anything unexpected.
+		fibonacci_heap(const fibonacci_heap& other) = delete;
+		fibonacci_heap(fibonacci_heap&& other) noexcept = delete;
+		fibonacci_heap& operator=(const fibonacci_heap& other) = delete;
+		fibonacci_heap& operator=(fibonacci_heap&& other) noexcept = delete;
+
+		// We return const pointer (node_handle_t).
+		node_t insert(T&& value, const int_least32_t priority) {
+			auto new_node = new node(value);
+			new_node->priority = priority;
+			// This follows basic way of merging this heap
+			// with new heap with only one new_node inside.
+			// But practically it is useless to create new
+			// complete heap, we can use just private merge.
+			node::merge(min_node, new_node);
+			min_node = (min_node && min_node->priority < priority) ? min_node : new_node;
+			++elements_count;
+			return new_node;
 		}
 
-		explicit node(T&& val) : value(std::move(val)) {
+		// As fast as possible (just C++ things).
+		// We return const pointer (node_handle_t).
+		node_t insert(const T& value, const int_least32_t priority) {
+			auto new_node = new node(value);
+			new_node->priority = priority;
+			// This follows basic way of merging this heap
+			// with new heap with only one new_node inside.
+			// But practically it is useless to create new
+			// complete heap, we can use just private merge.
+			node::merge(min_node, new_node);
+			min_node = (min_node && min_node->priority < priority) ? min_node : new_node;
+			++elements_count;
+			return new_node;
 		}
 
-		explicit node(const T& val) : value(val) {
+		node_t find_min() const noexcept {
+			return min_node;
 		}
 
-		// Append to_append on the right side of this.
-		node* append_right(node* to_append) {
-			to_append->left = this;
-			to_append->right = this->right;
-			this->right = to_append;
-			to_append->right->left = to_append;
-			return to_append;
-		}
-
-		// Append to_append on the left side of this.
-		node* append_left(node* to_append) {
-			to_append->right = this;
-			to_append->left = this->left;
-			this->left = to_append;
-			to_append->left->right = to_append;
-			return to_append;
-		}
-
-		// Remove this from the linked list of its neighbors and return handle to the linked list.
-		node* remove_from_neighbors() {
-			// Trivial case of linked list with one element.
-			if (right == this)
+		// Return min node with correctly left and right pointers set to node
+		// and child as nullptr. Sets this min_node to new min node in heap.
+		node* delete_min() {
+			if (!min_node)
 				return nullptr;
-			auto handle = right;
-			right->left = left;
-			left->right = right;
-			left = right = this;
-			return handle;
+			--elements_count;
+			// Update statistics (just step count).
+			fun_(false, true);
+			auto heap_minimum_ptr = min_node;
+			// Returns new heap LL of trees!
+			min_node = detach_min_and_level_min_children_up();
+			consolidate();
+			// Find new min.
+			if (min_node) {
+				auto min_it = min_node;
+				list_foreach(min_node, [&min_it](auto it) {
+					assert(!it->parent);
+					assert(!it->marked);
+					if (it->priority < min_it->priority)
+						min_it = it;
+				});
+				min_node = min_it;
+			}
+			return heap_minimum_ptr;
+		}
+
+		void decrease(node* node, int_least32_t new_priority) {
+			if (!node)
+				return;
+			node->priority = new_priority;
+			if (node->is_root()) {
+				if (min_node->priority >= new_priority)
+					min_node = node;
+				return;
+			}
+			if (node->parent->priority <= node->priority)
+				return;
+			cut(node);
+			// Update statistics (step_(naive_)decrease_count).
+			fun_(true, true);
+		}
+
+		fibonacci_heap merge(fibonacci_heap&& heap2) {
+			min_node = (min_node <= heap2.min_node) ? min_node : heap2.min_node;
+			node::merge(min_node, heap2.min_node);
+			elements_count += heap2.elements_count;
+			heap2.min_node = nullptr;
+			return this;
+		}
+
+		node* remove_node(node* node) {
+			// I could easily avoid using STL, but this is a trifle and
+			// much more readable and better maintainable.
+			decrease(node, std::numeric_limits<int_least32_t>::min());
+			--elements_count;
+			return delete_min();
+		}
+
+		void clear() {
+			// Simple and asymptotically slower than necessary, I know. But faster to write
+			// than DFS collection of nodes and then deleting one by one.
+			auto n = delete_min();
+			while (n) {
+				delete n;
+				n = delete_min();
+			}
+			elements_count = 0;
+			min_node = nullptr;
+		}
+
+		virtual ~fibonacci_heap() {
+			clear();
+		}
+
+	private:
+		// Put min_node children on min's top heap level LL, null min_node's child ptr.
+		node* detach_min_and_level_min_children_up() {
+			// For not losing pointer to the linked list.
+			auto min_node_neighbors = min_node->remove_from_neighbors();
+			// Remove parental pointers from children (min_node has still pointer to the child list).
+			// Update statistics ((naive_)delete_min_count).
+			list_foreach(min_node->child, [&](auto it) { it->parent = nullptr; it->marked = false; fun_(false, false); });
+			// Join top heap level linked list with minimum's children and null min's child.
+			auto heap_trees = node::merge(min_node->child, min_node_neighbors);
+			min_node->child = nullptr;
+			min_node->rank = 0;
+			return heap_trees;
+		}
+
+		// Removes node with its subtree from the tree it belongs to and merges it with top level heap LL.
+		void cut(node* node) {
+			if (!node || node->is_root())
+				return;
+			// Update statistics ((naive_)decrease_count).
+			fun_(true, false);
+			auto node_parent = node->parent;
+			node_parent->child = node->remove_from_neighbors();
+			--(node_parent->rank);
+			node->marked = false;
+			node->parent = nullptr;
+			node::merge(min_node, node);
+			if (!naive_implementation) {
+				if (node_parent->marked)
+					cut(node_parent);
+			}
+			if (!node_parent->is_root())
+				node_parent->marked = true;
+		}
+
+		void consolidate() {
+			// Trivial cases: heap has none or one node.
+			if (!min_node || !min_node->has_neighbors())
+				return;
+			const auto size = 100; //static_cast<std::size_t>(ceil(log2(elements_count + 1)) + 1);
+
+			// Within consolidation we reconnect tree pointers, so its safer to hold trees separately.
+			std::size_t tree_num = 0;
+			auto trees = new node*[size];
+			list_foreach(min_node, [&trees, &tree_num](auto it) {
+				trees[tree_num] = it; 
+				++tree_num;
+			});
+			for (std::size_t i = 0; i < tree_num; i++)
+				trees[i]->remove_from_neighbors();
+
+			auto pointers = new node*[size];
+			std::memset(pointers, 0, size * sizeof(node*) / sizeof(unsigned char));
+
+			const auto join_trees = [&](node* t1, node* t2) {
+				assert(!t1->has_neighbors() && !t2->has_neighbors());					
+				if (t1->priority < t2->priority) {
+					t2->parent = t1;
+					t1->child = node::merge(t1->child, t2);
+					++(t1->rank);
+					return t1;
+				}
+				t1->parent = t2;
+				t2->child = node::merge(t2->child, t1);
+				++(t2->rank);
+				return t2;
+			};
+
+			// For each tree in the heap that has at least two trees.
+			for (std::size_t i = 0; i < tree_num; i++) {
+				auto tree = trees[i];
+				auto rank = tree->rank;
+				while (pointers[rank]) {
+					// Update statistics ((naive_)delete_min_count).
+					fun_(false, false);
+					tree = join_trees(pointers[rank], tree);
+					pointers[rank] = nullptr;
+					++rank;
+				}
+				pointers[rank] = tree;
+			}
+			// Recreate the array into another valid linked list.
+			node* iterator = nullptr;
+			for (std::size_t i = 0; i < size; i++) {
+				if (pointers[i]) {
+					assert(!pointers[i]->has_neighbors());
+					iterator = node::merge(iterator, pointers[i]);
+				}
+			}
+			min_node = iterator;
+			delete[] pointers;
+			delete[] trees;
 		}
 	};
-public:
-	using priority_t = int_least32_t;
-	using rank_t = uint_least32_t;
-	using node_handle_t = node* const;
-
-	node* min_node = nullptr;
-	uint_least32_t elements_count = 0;
-
-	fibonacci_heap() = default;
-
-	// We return const pointer (node_handle_t).
-	node_handle_t insert(const T& value) {
-		auto new_node = new node(value);
-		// This follows basic way of merging this heap
-		// with new heap with only one new_node inside.
-		// But practically it is useless to create new
-		// complete heap, we can use just private merge.
-		min_node = merge(min_node, new_node);
-		++elements_count;
-		return new_node;
-	}
-
-	node_handle_t find_min() {
-		return min_node;
-	}
-
-	// Return min node with correctly left and right pointers set to node
-	// and child as nullptr. Sets this min_node to new min node in heap.
-	node* extract_min() {
-		if (!min_node)
-			return nullptr;
-		auto minimum = min_node;
-		// For not losing pointer to the linked list.
-		auto temp_handle = min_node->remove_from_neighbors();
-		// Remove parental pointers from children.
-		auto ptr = min_node->child;
-		if (ptr) {
-			while (ptr->right != min_node->child)
-				ptr->parent = nullptr;
-			// Hold only one child.
-			min_node->child->parent = nullptr;
-		}
-		// Join top heap level linked list with minimum's children and null min's child.
-		temp_handle = merge(temp_handle, min_node->child);
-		min_node->child = nullptr;
-		// The heap represented by min_node now shows temp_handle linked list.
-		min_node = temp_handle;
-		consolidate();
-		// Find new min.
-		ptr = min_node;
-		auto min = min_node;
-		while (ptr->right != min_node) {
-			if (ptr->value < min->value)
-				min = ptr;
-#ifdef DEBUG
-			static_assert(!ptr->parent);
-			static_assert(!ptr->marked);
-#endif
-			ptr = ptr->right;
-		}
-		min_node = min;
-		// Update elements_count.
-		--elements_count;
-		return minimum;
-	}
-
-	void decrease(node* node, priority_t new_priority) {
-		if (!node)
-			return;
-		node->priority = new_priority;
-		if (node == min_node || node->parent->priority <= node->priority)
-			return;
-		cut(node);
-	}
-
-	fibonacci_heap merge(fibonacci_heap&& heap2) {
-		auto temp = std::move(heap2);
-		min_node = merge(min_node, heap2.min_node);
-		elements_count += temp.elements_count;
-		heap2.min_node = nullptr;
-		return this;
-	}
-
-	node* remove_node(node* node) {
-		// I could easily avoid using STL, but this is a trifle and
-		// much more readable and better maintainable.
-		decrease(node, std::numeric_limits<priority_t>::min());
-		--elements_count;
-		return extract_min();
-	}
-
-private:
-	// Removes node with its subtree from the tree it belongs to and merges it with top level heap LL.
-	void cut(node* node) {
-		if (!node)
-			return;
-		auto f = node->parent;
-		--(f->rank);
-		auto handle = node->remove_from_neighbors();
-		f->child = handle;
-		node->marked = false;
-		node->parent = nullptr;
-		merge(min_node, node);
-		if (f->marked)
-			fhcut(f);
-	}
-
-	// Merge two nodes' neighbors into one LL, return one random of the resulting nodes' list.
-	static node* merge(node* heap1, node* heap2) {
-		if (!heap1)
-			return heap2;
-		if (!heap2)
-			return heap1;
-		// It does not matter on the way how we merge the linked lists.
-		heap2->append_right(heap1);
-		return (heap1->priority < heap2->priority) ? heap1 : heap2;
-	}
-
-	void consolidate() {
-		// Solve trivial cases.
-		if (!min_node || min_node->right == min_node)
-			return;
-		// Create sufficiently large array for storing pointers and initialize it to nullptrs.
-		const size_t size = ceil(log2(elements_count + 1));
-		auto pointers = new node*[size];
-		std::memset(pointers, nullptr, size);
-
-		auto ptr = min_node;
-		// For each tree in the heap that has at least two trees.
-		while (ptr->right != min_node) {
-			auto rank = ptr->rank;
-			while (pointers[rank]) {
-				const auto join_trees = [](node* t1, node* t2) {
-					if (t1->value < t2->value) {
-						t1->child->append_right(t2);
-						t1->rank += t2->rank;
-						return t1;
-					}
-					t2->child->append_right(t1);
-					t2->rank += t1->rank;
-					return t2;
-				};
-				ptr = join_trees(pointers[rank], ptr);
-				pointers[rank] = nullptr;
-				++rank;
-			}
-			pointers[rank] = ptr;
-		}
-		// Recreate the array into another valid linked list.
-		for (size_t i = 1; i < size - 1; i++) {
-			pointers[i]->right = pointers[i + 1];
-			pointers[i]->left = pointers[i - 1];
-		}
-		pointers[0]->right = pointers[1];
-		pointers[0]->left = pointers[size - 1];
-		pointers[size - 1]->right = pointers[0];
-		pointers[size - 1]->left = pointers[size - 2];
-		delete[] pointers;
-	}
-};
-	
 }
 
 
--- fibonacci_heap/src/main.cpp
+++ fibonacci_heap/src/main.cpp
@@ -1,7 +1,150 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "fib_heap.h"
 
 using namespace fh;
+using namespace std;
 
-int main(int argc, char* argv[]) {
-	auto heap = fibonacci_heap<int>();
+template<typename Out>
+void split(const string &s, const char& delimiter, Out&& result) {
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, delimiter)) {
+		if (!item.empty())
+			*(result++) = item;
+	}
+}
+
+vector<string> split(const string &s, const char& delimiter) {
+	vector<string> elems;
+	split(s, delimiter, back_inserter(elems));
+	return elems;
+}
+
+template<typename T>
+void reset_vector(T& vec, const size_t size) {
+	vec.clear();
+	vec.reserve(size + 1);
+	vec.insert(vec.end(), size + 1, nullptr);
+}
+
+int main(int, char* argv[]) {
+	uint_least64_t delete_min_count = 0;
+	uint_least64_t decrease_count = 0;
+	uint_least64_t naive_delete_min_count = 0;
+	uint_least64_t naive_decrease_count = 0;
+	uint_least64_t steps_delete_min_count = 0;
+	uint_least64_t steps_decrease_count = 0;
+	uint_least64_t steps_naive_delete_min_count = 0;
+	uint_least64_t steps_naive_decrease_count = 0;
+
+	auto naive = false;
+	auto fun = [&](const bool decrease, const bool operation_end) {
+		if (decrease) {
+			if (operation_end) {
+				if (naive)
+					++steps_naive_decrease_count;
+				else
+					++steps_decrease_count;
+			}
+			if (naive)
+				++naive_decrease_count;
+			else
+				++decrease_count;
+		}
+		if (operation_end) {
+			if (naive)
+				++steps_naive_delete_min_count;
+			else
+				++steps_delete_min_count;
+		}
+		if (naive)
+			++naive_delete_min_count;
+		else
+			++delete_min_count;
+	};
+	using heap_t = fibonacci_heap<int, decltype(fun)>;
+
+	heap_t heap_classic(fun);
+	heap_t heap_naive(fun);
+	heap_naive.naive_implementation = true;
+
+	string line;
+	// Vector used for clarity and debugging instead of a pointer to a regularly
+	// re-allocated array of node handles.
+	vector<heap_t::node_t> remembered_nodes_naive;
+	vector<heap_t::node_t> remembered_nodes_classic;
+	size_t last_n = 0;
+	auto first = true;
+	ofstream ofs{ argv[1] };
+	ifstream ifs{ "rand_uniform.txt" };
+
+	while (!getline(ifs, line).eof()) {
+		//cout << line << endl;
+		auto tokens = split(line, ' ');
+		if (tokens[0] == string("#")) {
+			if (!first) {
+				ofs << last_n << " ";
+				ofs << delete_min_count / steps_delete_min_count << " ";
+				ofs << decrease_count / steps_decrease_count << " ";
+				ofs << naive_delete_min_count / steps_naive_delete_min_count << " ";
+				ofs << naive_decrease_count / steps_decrease_count << endl;
+				ofs << flush;
+				cout << "FUCK THAT SHIT!" << endl;
+			}
+			// reset counters
+			delete_min_count = 0;
+			decrease_count = 0;
+			naive_delete_min_count = 0;
+			naive_decrease_count = 0;
+			steps_delete_min_count = 0;
+			steps_decrease_count = 0;
+			steps_naive_delete_min_count = 0;
+			steps_naive_decrease_count = 0;
+			// prepare new run
+			last_n = stoi(tokens[1]);
+			heap_classic.clear();
+			reset_vector(remembered_nodes_classic, last_n);
+			heap_naive.clear();
+			reset_vector(remembered_nodes_naive, last_n);
+			first = false;
+		}
+		else if (tokens[0] == string("I")) {
+			const auto identification = stoi(tokens[1]);
+			const auto priority = stoi(tokens[2]);
+			naive = false;
+			auto handle = heap_classic.insert(identification, priority);
+			remembered_nodes_classic.at(identification) = handle;
+			naive = true;
+			handle = heap_naive.insert(identification, priority);
+			remembered_nodes_naive.at(identification) = handle;
+		}
+		else if (tokens[0] == string("M")) {
+			naive = false;
+			const auto min = heap_classic.delete_min();
+			if (min) remembered_nodes_classic.at(min->value) = nullptr;
+			delete min;
+			naive = true;
+			const auto min2 =  heap_naive.delete_min();
+			if (min2) remembered_nodes_naive.at(min2->value) = nullptr;
+			delete min2;
+		}
+		else { // if (tokens[0] == string("D"))
+			const auto identification = stoi(tokens[1]);
+			const auto priority = stoi(tokens[2]);
+			if (remembered_nodes_classic.at(identification) != nullptr &&
+				remembered_nodes_classic.at(identification)->priority >= priority) {
+				naive = false;
+				heap_classic.decrease(remembered_nodes_classic.at(identification), priority);
+			}
+			if (remembered_nodes_naive.at(identification) != nullptr &&
+				remembered_nodes_naive.at(identification)->priority >= priority) {
+				naive = true;
+				heap_naive.decrease(remembered_nodes_naive.at(identification), priority);
+			}
+		}
+	}
 }
